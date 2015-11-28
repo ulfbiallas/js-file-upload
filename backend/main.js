278594bf8c9f1459018f4e0cfbd8da3015fd5144
ulfbiallas <ulfbiallas@googlemail.com>
2015-11-28 18:16:47 +0100

    implemented upload on server-side

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,29 @@
 var restify = require('restify')
+var fs = require('fs');
 
 var server = restify.createServer()
+server.use(restify.bodyParser());
+
+
+server.post('/', function(req, res, next) {
+  var filename = req.files.data.name
+  var tempfilePath = req.files.data.path
+
+  var source = fs.createReadStream(tempfilePath);
+  var dest = fs.createWriteStream("uploads/" + filename);
+
+  dest.on("error", function() {
+    res.send(500);
+    return next();
+  });
+
+  dest.on("close", function(ex) {
+    res.send(201);
+    return next();
+  });
+
+  source.pipe(dest);
+});
+
 
 server.listen(8080)
